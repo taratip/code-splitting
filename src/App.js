cc57f6d28cbf6d93d1fb206e4c7d5a63ce7f4948
Tara Jaikaeo <tjaikaeo@gmail.com>
2025-04-03 16:19:49 -0400

    update to use lazy

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import './App.css';
 
 import Page1 from '../components/Page1';
+
+const Page2View = lazy(() => import('../components/Page2'));
+const Page3View = lazy(() => import('../components/Page3'));
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
             route: 'page1',
-            component: null,
         }
     }
     onRouteChange = (route) => {
-        if (route === 'page1') {
-            this.setState({ route: route });
-        } else if (route === 'page2') {
-            import('../components/Page2').then((Page2) => {
-                this.setState({ route: route, component: Page2.default });
-            });
-        } else if (route === 'page3') {
-            import('../components/Page3').then((Page3) => {
-                this.setState({ route: route, component: Page3.default });
-            });
-        }
+        this.setState({ route: route });
     }
     render() {
         if (this.state.route === 'page1') {
             return <Page1 onRouteChange={this.onRouteChange} />
+        } else if (this.state.route === 'page2') {
+            return (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Page2View onRouteChange={this.onRouteChange} />
+                </Suspense>
+            );
         } else {
-            return <this.state.component onRouteChange={this.onRouteChange} />
+            return (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Page3View onRouteChange={this.onRouteChange} />
+                </Suspense>
+            );
         }
     }
 }
